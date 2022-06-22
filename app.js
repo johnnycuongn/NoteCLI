4a06ca9059139bd2ed2d1ed6233c49753a4a5ed3
Johnny Nguyen <cuongdn2001@gmail.com>
2022-06-23 01:54:44 +0700

    cli yargs works

--- app.js
+++ app.js
@@ -2,52 +2,34 @@ const yargs = require("yargs");
 const fs = require('fs')
 const notes = require('./notes')
 
-// console.log(process.argv);
-
-
-yargs.command('add', true, {
-   text: {
-        describe: 'Note body',
-        demandOption: true,
-        type: 'string',
-        alias: 't'
+let argv = yargs.command({
+    command: 'add',
+    describe: 'Adds two number',
+    builder: {
+        text: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string',
+            alias: 't'
+        }
+    },
+    // Function for your command
+    handler(argv) {
+        console.log('adding note')
     }
-}, function (argv) {
-    // notes.addSingleNote(argv.text)
-    console.log('adding note')
 })
+.command('remove', true, {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string',
+            alias: 't'
+        }
+    }, (argv) => {
+        console.log('Removing note ' + argv.title)
+    })
+    .command('list', true, (argb) => {
+        console.log('Listing notes on date');
+    }).help().argv;
 
-
-
-const removeArgv = yargs.command('remove', true, {
-    title: {
-        describe: 'Note title',
-        demandOption: true,
-        type: 'string',
-        alias: 't'
-    }
-}, (argv) => {
-    console.log('Removing note ' + argv.title)
-}).argv
-
-const listArgv = yargs.command('list', true, (argv) => {
-    console.log('Listing all notes')
-}).parse()
-
-const getArgv = yargs.command('get', true, {
-    title: {
-        describe: 'Note title',
-        demandOption: true,
-        type: 'string',
-        alias: 't'
-    }
-}, (argv) => {
-    console.log('Getting ' + argv.title)
-}).parse()
-
-module.exports = {
-// addArgv: addArgv,
-removeArgv,
-listArgv,
-getArgv
-}
+// yargs.option('t', {alias: 'text', describe: 'Note', type: 'string', demandOption: true})
--- bin/index.js
+++ bin/index.js
@@ -1,24 +1,11 @@
 #! /usr/bin/env node
 
-// require('../app')
-const yargs = require("yargs");
+require('../app')
 const notes = require('../notes');
-const utils = require('../utils')
+const utils = require('../utils');
 
-// yargs.command('add', true, {
-//     text: {
-//          describe: 'Note body',
-//          demandOption: true,
-//          type: 'string',
-//          alias: 't'
-//      }
-//  }, function (argv) {
-//      // notes.addSingleNote(argv.text)
-//      console.log('adding note')
-//  }).help()
  
-
-if (yargs.argv._) {
-    const sentence = utils.parseSentence(yargs.argv._)
-    notes.addSingleNote(sentence)
-}
+// if (yargs.argv._) {
+//     const sentence = utils.parseSentence(yargs.argv._)
+//     notes.addSingleNote(sentence)
+// }
