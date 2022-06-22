542bf200dd72aae7a669ea3be17aa3efc60667a1
Johnny Nguyen <cuongdn2001@gmail.com>
2022-06-23 00:50:26 +0700

    update add notes to markdown

--- notes.js
+++ notes.js
@@ -1,5 +1,6 @@
 
 const fs = require('fs')
+const { getDate, getHourMinute } = require('./utils')
 
 // async function start() {
 //     console.log(await fs.readFile('./notes.json', { encoding: 'utf-8' }, (err, dataBuffer) => {
@@ -8,10 +9,10 @@ const fs = require('fs')
 // }
 
 // start()
-debugger
-fs.readFile('./notes.json', 'utf-8', (err, data) => {
-    console.log('JSON NOTES: ' + data)
-})
+// debugger
+// fs.readFile('./notes.json', 'utf-8', (err, data) => {
+//     console.log('JSON NOTES: ' + data)
+// })
 
 
 
@@ -19,21 +20,58 @@ function getNote(title) {
     console.log('Gettiong note')
 }
 
-async function addNote(title, body) {
-    console.log('adding note');
-    const notes = await loadNotes()
-    debugger
-    notes.push({
-        title: title,
-        body: body
+async function addSingleNote(body) {
+    const date = getDate();
+    const currentHourMin = getHourMinute();
+
+    const endFile = `./data/${date}.md`
+    if (!fs.existsSync(endFile)) {
+        fs.writeFile(endFile, `# ${getDate('/')}\n`, (err) => {
+            if (err) return console.error(err)
+        })
+    }
+
+    let addedText = `## ${currentHourMin}\n${body}\n\n`;
+
+    fs.readFile(endFile, 'utf-8', (err, data) => {
+        var arr = data.split("\n").filter(s => s);
+
+        for (var i = arr.length-1; i >= 0; i--) {
+            if (arr[i].startsWith('##')) {
+                const lastHourMinute = arr[i].split(' ')[1].trim()
+
+                if (lastHourMinute == currentHourMin) {
+                    addedText = `${body}\n`
+                } else {
+                    addedText =  `## ${currentHourMin}\n${body}\n`
+                }
+                break;
+            }
+        }
+        fs.appendFile(endFile, addedText, (err) => {
+            if (err) return console.error('Error adding note')
+            console.log('Successfully adding data')
+        })
     })
-    debugger
-    const writeData = JSON.stringify(notes)
-    fs.writeFile('./notes.json', writeData, (err) => {
-        if (err) { console.error('Error writing data') }
-        console.log('Successfully adding ' + title)
-    })
+
+
+
 }
+
+// async function addNote(title, body) {
+//     const notes = await loadNotes()
+//     debugger
+//     notes.push({
+//         title: title,
+//         body: body
+//     })
+//     debugger
+//     const writeData = JSON.stringify(notes)
+//     fs.writeFile('./notes.json', writeData, (err) => {
+//         if (err) { console.error('Error writing data') }
+//         console.log('Successfully adding ' + title)
+//     })
+// }
 
 const loadNotes = async () => {
     return new Promise((resolve, reject) => {
@@ -51,5 +89,5 @@ const loadNotes = async () => {
 
 module.exports = {
     getNote: getNote,
-    addNote: addNote
+    addSingleNote: addSingleNote
 }
